89cabdda379d30858dd33c4580be1da48a1652f2
Antonio Vanegas <hpsaturn@gmail.com>
2016-12-20 08:33:04 -0500

    add general config class and flag set for debugging

---
+++ wishbone/src/main/java/com/example/androidthings/simplepio/Config.java
@@ -0,0 +1,8 @@
+package com.example.androidthings.simplepio;
+
+/**
+ * Created by Antonio Vanegas @hpsaturn on 12/19/16.
+ */
+public class Config {
+    public static final boolean DEBUG = true;
+}
